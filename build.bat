60dde4f7c40db42222daeef79850d349c2f5cc54
zoogie <stackrestacker@gmail.com>
2023-05-30 04:11:52 -0500

    Support 11.17 US (v1.1)

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 rem goto end
-python scripts/buildVersion.py "11.15.0-47U"
+python scripts/buildVersion.py "11.17.0-50U"
+python scripts/buildVersion.py "N11.17.0-50U"
+goto end
 python scripts/buildVersion.py "11.15.0-47E"
 python scripts/buildVersion.py "11.15.0-47J"
 
-python scripts/buildVersion.py "N11.15.0-47U"
 python scripts/buildVersion.py "N11.15.0-47E"
 python scripts/buildVersion.py "N11.15.0-47J"
 :end
